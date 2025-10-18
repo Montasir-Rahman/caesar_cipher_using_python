5d8762b565b1ca6eaafd48ca5fbba40c3cc5fd8e
Montasir-Rahman <montasirrahmanhridoy@gmail.com>
2025-10-18 22:16:41 +0600

    Comments Added

--- caesar_cipher/caesar_cipher.py
+++ caesar_cipher/caesar_cipher.py
@@ -1,4 +1,4 @@
-
+# Function for Caesar Cipher
 def caesar(message, offset, direction = 1):
     alphabet = "abcdefghijklmnopqrstuvwxyz"
     final_message = ''
@@ -17,11 +17,13 @@ def caesar(message, offset, direction = 1):
             
     return final_message
 
+# Function to write encrypted message in a text file  
 def write_encrypted_to_file(text, filename = "encrypted.txt"):
     with open(filename, 'w') as file:
         file.write(text)
     print(f"Encrypted text written to {filename}")
 
+# Function to encrypt message
 def encrypt():
     text = input("Enter text to encrypt: ")
     key = int(input("Enter any integer number: "))
@@ -31,7 +33,8 @@ def encrypt():
     
     print(f"Original Message: {text}")
     print(f"Encrypted Message: {final_text}")
-    
+
+# Function to read encrypted message from the text file    
 def read_encrypted_from_file(filename = "encrypted.txt"):
     try:
         with open(filename, 'r') as file:
@@ -40,12 +43,14 @@ def read_encrypted_from_file(filename = "encrypted.txt"):
     except FileNotFoundError:
         print(f"File {filename} not found")
         return None
-    
+ 
+# Function to write decrypted message in a text file   
 def write_decrypted_to_file(text, filename = "decrypted.txt"):
     with open(filename, 'w') as file:
         file.write(text)
     print(f"Decrypted text written to {filename}")
 
+# Function to decrypt message
 def decrypt():
     text = read_encrypted_from_file()
     key = int(input("Enter your integer key: "))
@@ -56,6 +61,7 @@ def decrypt():
     print(f"Encrypted Message: {text}")
     print(f"Decrypted Message: {final_text}")
 
+# Main function
 def main():
     print("Welcome to Caesar Cipher")
     print("Enter-1: Encryption Service")
